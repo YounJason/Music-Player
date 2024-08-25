3f1847266512fca04198be739459ed431664d1a6
윤재선 (Youn Jason) <58214922+YounJason@users.noreply.github.com>
2024-08-25 21:28:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -355,7 +355,7 @@ async function LoadLyric(artist, title) {
 }
 
 $(document).ready(function () {
-    fetch('/.netlify/functions/translate', {
+    fetch('/.netlify/functions/deepl', {
         method: 'POST',
         body: JSON.stringify({
             text: "번역할 텍스트",
