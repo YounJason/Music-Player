0e7b0b1e1092291aeb53ebc79a1f4e34ca9a975d
Youn Jason <58214922+YounJason@users.noreply.github.com>
2024-08-27 23:20:11 +0900

    Add files via upload

---
+++ netlify-functions/googletranslate.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+    const { text } = JSON.parse(event.body);
+    const url = `https://translation.googleapis.com/language/translate/v2?key=AIzaSyCVJUFSvH5CwN5fVLIAJg_okVDhtOmAQLo`;
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            q: text,
+            target: 'ko'
+        })
+    });
+
+    const data = await response.json();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+    };
+};
---
+++ netlify-functions/spotify.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+    const SpotifyAuthParams = new URLSearchParams();
+    SpotifyAuthParams.append('grant_type', 'client_credentials');
+    SpotifyAuthParams.append('client_id', '4242dcadb4c94cc7930152930ff1eeba');
+    SpotifyAuthParams.append('client_secret', 'c7dfe97221754f0fa8ac47ace42c28bb');
+
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: SpotifyAuthParams
+    });
+
+    const data = await response.json();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+    };
+};
