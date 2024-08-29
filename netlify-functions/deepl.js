cb5f0797427070eb63074ddcdef07f237450f845
Youn Jason <58214922+YounJason@users.noreply.github.com>
2024-08-30 00:16:07 +0900

    Add files via upload

--- netlify-functions/deepl.js
+++ netlify-functions/deepl.js
@@ -1,28 +1,21 @@
 exports.handler = async function(event, context) {
-    // fetch를 동적 import로 불러오기
     const fetch = (await import('node-fetch')).default;
-  
-    // POST 요청으로 들어온 데이터 확인
+
     const { text } = JSON.parse(event.body);
-  
-    // DeepL API 키 가져오기
     const apiKey = process.env.DEEPL_API_KEY;
-    
-    // DeepL API 엔드포인트 및 요청 파라미터 설정
     const url = 'https://api-free.deepl.com/v2/translate';
     const params = new URLSearchParams({
-      auth_key: apiKey,
-      text: text,
-      target_lang: 'KO' // 한국어로 번역
+        auth_key: apiKey,
+        text: text,
+        target_lang: 'KO'
     });
 
-      // DeepL API 호출
-      const response = await fetch(`${url}?${params}`);
-      const data = await response.json();
-  
-      // 번역된 텍스트 반환
-      return {
-        body: data,
-      };
-  };
-  
+    const response = await fetch(`${url}?${params}`);
+    const data = await response.json();
+
+    // 번역된 텍스트 반환 (JSON 객체를 문자열로 변환)
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data), // 여기서 data를 문자열로 변환
+    };
+};
